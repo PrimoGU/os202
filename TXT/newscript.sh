de23bcc28694867034fd7303edbd03020616a62c
Primo Giancarlo Uneputty <primo.uneputty@gmail.com>
2020-10-05 19:24:18 +0700

    Created new bash script

---
+++ TXT/newscript.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Primo Giancarlo Uneputty - 1806205180
+# https://github.com/PrimoGU/os202/
+# Script to add, commit and push to GitHub Repo.
+
+echo "Enter your commit message"
+read message
+git pull
+git add -A
+git commit -m"${message}"
+if [ -n "$(git status - porcelain)" ];
+then
+	echo "It is clean, nothing to commit..."
+else
+	git status
+	echo "Pushing data to remote server..."
+	git push
+fi
